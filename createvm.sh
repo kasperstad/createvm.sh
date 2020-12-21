252723bf6bdce72b543caa48d7592849eb477427
kasperstad <contact@kasperstad.dk>
2020-12-21 17:03:27 +0100

    multios

--- createvm.sh
+++ createvm.sh
@@ -210,7 +210,7 @@ VMID=$(pvesh get /cluster/nextid)
 
 if [ -f "${VM_CLOUDIMG_PATH}.md5sum" ]; then
     wget -o /dev/null -O "${VM_CLOUDIMG_TEMPLATEPATH}/MD5SUMS" ${VM_CLOUDIMG_MD5SUMS}
-    grep "$(basename $VM_CLOUDIMG_URL)" "${VM_CLOUDIMG_TEMPLATEPATH}/MD5SUMS" > "${VM_CLOUDIMG_PATH}.md5sum.new"
+    grep "$(basename $VM_CLOUDIMG_URL)" "${VM_CLOUDIMG_TEMPLATEPATH}/MD5SUMS" | awk '{print $1}' > "${VM_CLOUDIMG_PATH}.md5sum.new"
     if [ "$(cat ${VM_CLOUDIMG_PATH}.md5sum)" -ne "$(cat ${VM_CLOUDIMG_PATH}.md5sum.new)" ]; then
         echo "[$BASENAME]: newer image available, downloading"
         wget --show-progress -o /dev/null -O $VM_CLOUDIMG_PATH $VM_CLOUDIMG_URL
