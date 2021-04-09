12fcc601e3b7c807cb21613b4126f62a55fdfc09
Kasper Stad <git@kasperstad.dk>
2021-04-09 19:50:22 +0200

    created light version of vmbuilder.sh

---
+++ vmbuilder-light.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+set -e
+
+# MIT License
+#
+# Copyright (c) 2020 Kasper Stad
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+# Script Basename
+BASENAME=$(basename $0)
+
+# Help meassage
+function get_help()
+{
+    echo
+    echo "Usage: $BASENAME <parameters> ..."
+    echo
+    echo "Parameters:"
+    echo "    -c, --cores             CPU Cores that will be assigned to the VM (default: 1)"
+    echo "    --disk-size             Size of the VM disk in GB (default: 20)"
+    echo "    -h, --help              Show this help message."
+    echo "    -i, --ip-address        (required) IP Address of this VM in CIDR format (eg. 192.168.1.2/24)"
+    echo "    --id                    Proxmox VM ID"
+    echo "    -m, --memory            Memory that will be allocated to the VM in MB (default: 1024)"
+    echo "    -n, --name              (required) Name of the VM without spaces, dots and other ambiguous characters"
+    echo
+    exit 1
+}
+
+# This script needs root permissions to run, check that
+if [ "$EUID" -ne 0 ]; then
+    echo -e "[$BASENAME]: \033[0;31mError: You must run this script as root\033[0m"
+    exit 1
+fi
+
+# Get Help if you don't specify any arguments...
+if [ ${#} -eq 0 ]; then
+    get_help
+fi
+
+# Parse all parameters
+while [ ${#} -gt 0 ]; do
+    case "${1}" in
+        -c|--cores)
+            VM_CORES=$2
+            shift
+            shift
+            ;;
+        --disk-size)
+            VM_DISK_SIZE="$2"
+            shift
+            shift
+            ;;
+        -h|--help)
+            get_help
+            ;;
+        -i|--ip-address)
+            VM_IP_ADDRESS="$2"
+            shift
+            shift
+            ;;
+        --id)
+            VMID=$2
+            shift
+            shift
+            ;;
+        -m|--memory)
+            VM_MEMORY=$2
+            shift
+            shift
+            ;;
+        -n|--name)
+            VM_NAME="$2"
+            if [[ $VM_NAME == *['!'@#\$%^\&*()\_+\']* ]];then
+                echo -e "[$BASENAME]: \033[0;31mError: Specified hostname is invalid\033[0m"
+                exit 1
+            fi
+            shift
+            shift
+            ;;
+        *)
+            get_help
+            ;;
+    esac
+done
+
+# Ubuntu Cloud Image Variables
+VM_CLOUDIMG_URL="https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img"
+
+# Default values if they wasn't defined as parameters
+# CHANGE THESE VALUES AS NEEDED IF YOU LIKE!
+VM_CORES=${VM_CORES:-1}
+VM_DISK_SIZE=${VM_DISK_SIZE:-20}
+VM_DNS_SERVER=${VM_DNS_SERVER:-"8.8.8.8"}
+VM_DOMAIN=${VM_DOMAIN:-"localdomain"}
+VM_MEMORY=${VM_MEMORY:-1024}
+VM_NET_BRIDGE=${VM_NET_BRIDGE:-"vmbr0"}
+VM_NET_VLAN=${VM_NET_VLAN:-2}
+VM_SNIPPETS_STORAGE_NAME=${VM_SNIPPETS_STORAGE_NAME:-"local"}
+VM_SNIPPETS_STORAGE_PATH=${VM_SNIPPETS_STORAGE_PATH:-"/var/lib/vz/snippets"}
+VM_SSH_KEYFILE=${VM_SSH_KEYFILE:-"${HOME}/.ssh/id_rsa.pub"}
+VM_STORAGE=${VM_STORAGE:-"local-lvm"}
+
+# Get Help if you don't specify required parameters (yes I know I'm a little demanding ;) )...
+if [[ -z $VM_NAME || -z $VM_IP_ADDRESS || -z $VM_SSH_KEYFILE || -z $VMID ]]; then
+    get_help
+fi
+
+# Cloudimage base variables
+VM_CLOUDIMG_NAME=$(basename $VM_CLOUDIMG_URL)
+VM_CLOUDIMG_FULL_PATH="/tmp/${VM_CLOUDIMG_NAME}"
+
+echo -e "[$BASENAME]: \033[1;32mdownloading image...\033[0m"
+wget --show-progress -o /dev/null -O $VM_CLOUDIMG_FULL_PATH $VM_CLOUDIMG_URL
+
+# Create the new VM
+qm create $VMID --name $VM_NAME --cores $VM_CORES --memory $VM_MEMORY -ostype l26
+
+# Import the image to the storage
+qm importdisk $VMID $VM_CLOUDIMG_FULL_PATH $VM_STORAGE | egrep -v '^transferred:'
+
+# Attach the disk to scsi0 bus
+qm set $VMID --scsihw virtio-scsi-pci --scsi0 $VM_STORAGE:vm-$VMID-disk-0,discard=on
+
+# Resize the imported disk
+qm resize $VMID scsi0 ${VM_DISK_SIZE}G
+
+# Set the default boot disk to be the newly imported disk
+qm set $VMID --boot c --bootdisk scsi0
+
+# Add a cloud-init drive
+qm set $VMID --ide2 $VM_STORAGE:cloudinit
+
+# Add the SSH Keys to the server
+qm set $VMID --sshkey $VM_SSH_KEYFILE
+
+# Set the VM to use serial0 as the default vga device
+qm set $VMID --serial0 socket --vga serial0
+
+# Attach network interface to a bridge, and optionally set VLAN tag
+qm set $VMID --net0 virtio,bridge=$VM_NET_BRIDGE,tag=$VM_NET_VLAN,firewall=1
+
+# If no default gateway is defined, we're creating one
+if [ -z "${VM_GATEWAY}" ]; then
+    VM_GATEWAY="$(echo ${VM_IP_ADDRESS} | cut -d '.' -f -3).1"
+fi
+
+# Setup the network, DNS server and domain
+qm set $VMID --ipconfig0 ip=$VM_IP_ADDRESS,gw=$VM_GATEWAY
+qm set $VMID --nameserver $VM_DNS_SERVER --searchdomain $VM_DOMAIN
+
+# If we want the QEMU Guest agent enabled and installed, then we dump the proxmox generated user-data and append to the cicustom
+# We'll show a warning about that removes the availability to modify user-data through the WebUI
+VM_CICUSTOM_USER_DATA="${VM_SNIPPETS_STORAGE_PATH}/${VMID}.yml"
+echo -e "[$BASENAME]: \033[1;33mYou have choosen to install qemu-guest-agent on ${VM_NAME} (id: ${VMID}) automatically\033[0m"
+echo -e "[$BASENAME]: \033[1;33mThis prevents modifications to user-data through Proxmox WebUI, but allows you to edit this yaml user-data file instead:\033[0m"
+echo -e "[$BASENAME]: \033[1;33m  ${VM_CICUSTOM_USER_DATA}\033[0m"; sleep 1
+cat > $VM_CICUSTOM_USER_DATA << EOF
+$(qm cloudinit dump $VMID user)
+apt_reboot_if_required: True
+timezone: Europe/Copenhagen
+packages:
+  - qemu-guest-agent
+runcmd:
+  - systemctl enable qemu-guest-agent
+  - systemctl restart qemu-guest-agent
+EOF
+
+qm set $VMID --agent 1 --cicustom user="${VM_SNIPPETS_STORAGE_NAME}:snippets/${VMID}.yml"
+qm start $VMID
+
+rm -f ${VM_CLOUDIMG_FULL_PATH}
