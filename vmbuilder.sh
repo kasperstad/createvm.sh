36d043dca78c5afd4986cba0816007cce45ca00f
Kasper Stad <git@kasperstad.dk>
2021-06-11 00:36:33 +0200

    Update vmbuilder.sh

--- vmbuilder.sh
+++ vmbuilder.sh
@@ -224,7 +224,7 @@ timezone: Europe/Copenhagen
 apt:
   sources:
     docker.list:
-      source: deb [arch=amd64] https://download.docker.com/linux/ubuntu stable
+      source: deb [arch=amd64] https://download.docker.com/linux/ubuntu \$RELEASE stable
       keyid: 9DC858229FC7DD38854AE2D88D81803C0EBFCD88
 packages:
   - docker-ce
